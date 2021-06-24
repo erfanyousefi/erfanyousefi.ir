a9b8735befa4edacaa19823c9ed4376683f4f12d
Erfan Yousefi <coffeelearngroup@gmail.com>
2021-06-25 00:24:33 +0430

    add imageUpload middleware

---
+++ app/http/middleware/uploadImage.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+
+function getPath() {
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+    const day = new Date().getDate();
+    return `public/uploads/${year}/${month}/${day}`;
+}
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const directory = getPath()
+        mkdirp(directory).then(dir => cb(null, directory))
+    },
+    filename: (req, file, cb) => {
+        const newName = new Date.now();
+        const dir = getPath();
+        if (!fs.existsSync(dir + "/" + file.originalname)) {
+            cb(null, file.originalname)
+        } else {
+            cb(null, newName + "-" + file.originalname);
+        }
+    }
+});
+
+const imgStorage = multer({ storage })
+
+module.exports = storage;
